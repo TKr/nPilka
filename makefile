a4c98397b0f844a172e46ba1d16f51b53195d941
Paweł Pyrczak <pawel.pyrczak@kissdigital.pl>
2018-04-12 17:53:50 +0200

    import assets

---
+++ makefile
@@ -0,0 +1,11 @@
+help:
+	@echo " help - show this"
+	@echo " serve - run dev server"
+	@echo " prod - build prod (optimized) version"
+
+serve:
+	./ng serve --base-href=/
+
+prod:
+	./ng build --env=prod --prod --aot --buildOptimizer --no-sourcemaps --base-href=/
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+//    var gra = new game(new board(), new bot());
+//
+//    gra.plansza.addline(1,9,line_bottom); /// zaczynamy od dołu
+//    gra.plansza.addline(10,18,line_bottom_plus_1);
+//    for (var i=9; i<=63; i+=9) {
+//        gra.plansza.addline((10+i),(18+i),line);
+//    }
+//    gra.plansza.addline(82,90,line_top_minus_1);
+//    gra.plansza.addline(91,99,line_top);
+//    gra.plansza.addline(-4,-4,line_bramka_bottom);
+//    gra.plansza.addline(104,104,line_bramka_top);
+//    gra.plansza.render($('boisko'));
+//    gra.next_move();
+
+  }
 }
